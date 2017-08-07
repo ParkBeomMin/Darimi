e3bb22b4ecc2a6ebbb417f052568445683a3912c
jisung0920 <jisung0920@gmail.com>
2017-08-08 00:31:23 +0900

    list function

---
+++ app/src/main/java/com/example/jisung/darimi/Categol.java
@@ -0,0 +1,21 @@
+package com.example.jisung.darimi;
+
+/**
+ * Created by jeongjiseong on 2017. 8. 7..
+ */
+
+public class Categol {
+    private String cate_name;
+
+    public Categol(String cate_name) {
+        this.cate_name = cate_name;
+    }
+
+    public String getCate_name() {
+        return cate_name;
+    }
+
+    public void setCate_name(String cate_name) {
+        this.cate_name = cate_name;
+    }
+}
